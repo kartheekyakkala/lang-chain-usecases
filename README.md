30bc0a2e648dbc38f0e58e45a7d10588c66776ef
kartheek <50788154+kyk-001@users.noreply.github.com>
2023-06-11 13:00:59 +0000

    code enhancement

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 how to run the code:
   1. pip install -r requirements.txt
-  1. add your openapi key to env file and run main.py
+  1. add your openapi key to env file and run "streamlit run main.py"
--- save_file.py
+++ save_file.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 def save_uploaded_file(uploadedfile):
     current_directory = os.getcwd()
-    with open(os.path.join(current_directory, uploadedfile.name), "wb") as f:
+    with open(os.path.join(current_directory, "doc.txt"), "wb") as f:
         f.write(uploadedfile.getbuffer())
-    return st.success("Saved file :{} in Current working directory".format(uploadedfile.name))
+    return st.success("Saved file :{} in Current working directory".format("doc.txt"))
